4908f791e8b741d3637fdd10bc899ae53af54588
jalovisko <letovnn@gmail.com>
2021-03-11 00:45:48 -0500

    added an -all argument

--- ffmpeg-converter.sh
+++ ffmpeg-converter.sh
@@ -1,8 +1,17 @@
-for var in "$@"
-do
-	# Getting filename without extention
-	filename="${var%.*}"
-	# Converting using ffmpeg
-	ffmpeg -i "$var" -c:v copy -c:a libmp3lame -q:a 4 "${filename}.mp3"
-	echo "$var"
-done
+if [ "$1" = "-all" ]
+then
+	echo "$1"
+    for file in *.m4a;
+	do
+		filename="${file%.*}"
+		ffmpeg -i "$file" -c:v copy -c:a libmp3lame -q:a 4 "${filename}.mp3"
+	done
+else
+	for var in "$@"
+		do
+			# Getting filename without extention
+			filename="${var%.*}"
+			# Converting using ffmpeg
+			ffmpeg -i "$var" -c:v copy -c:a libmp3lame -q:a 4 "${filename}.mp3"
+		done
+fi
